81d208c8f08c2fd29055565aeb8f50df8093cf34
lschirripa <lucho.schirripa@gmail.com>
2022-04-28 00:38:02 -0300

    add expenses exs

---
+++ expenses.py
@@ -0,0 +1,13 @@
+###############LOOP RESOLUTION###############
+
+expenses = [10,20,30,40,50]
+
+# sum = 0
+# for valores in expenses:
+#     sum = sum + valores
+
+###############SUM FUNCTION RESOLUTION###############
+
+sum = sum(expenses)
+
+print("your total is $", sum, ", good luck with that!", sep='')
