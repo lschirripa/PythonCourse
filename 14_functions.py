5158f6ded03fb25982b8369dbf77d89366a24173
lschirripa <lschirripa@frba.utn.edu.ar>
2022-04-28 23:02:43 -0300

    feat: functions complete

---
+++ 14_functions.py
@@ -0,0 +1,11 @@
+#using local scope
+
+def greeting(name):
+    print("hello " + name)
+
+#main program
+
+name1 = input("enter your name\n")
+greeting(name1)
+name2 = input("enter your name\n")
+greeting(name2)
---
+++ 15_functionsAndOrder.py
@@ -0,0 +1,11 @@
+
+def addition(a,b):
+    return a + b
+
+def main():
+    v1 = float(input("enter the first value"))
+    v2 = float(input("enter the second value"))
+    result= addition(v1,v2)
+    print("the result is " + str(result))
+
+main()
