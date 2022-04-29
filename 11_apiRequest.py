4cca3867f1a4fa712970156f4d558ae6f8f913cf
lschirripa <lschirripa@frba.utn.edu.ar>
2022-04-28 21:24:41 -0300

    feat: now api gets data ordered

--- 11_apiRequest.py
+++ 11_apiRequest.py
@@ -6,3 +6,55 @@ json = response.json()
 
 for persons in json['people']:
     print(persons['name'])
+
+
+# {
+#    "message":"success",
+#    "number":11,
+#    "people":[
+#       {
+#          "name":"Raja Chari",
+#          "craft":"ISS"
+#       },
+#       {
+#          "name":"Tom Marshburn",
+#          "craft":"ISS"
+#       },
+#       {
+#          "name":"Kayla Barron",
+#          "craft":"ISS"
+#       },
+#       {
+#          "name":"Matthias Maurer",
+#          "craft":"ISS"
+#       },
+#       {
+#          "name":"Oleg Artemyev",
+#          "craft":"ISS"
+#       },
+#       {
+#          "name":"Denis Matveev",
+#          "craft":"ISS"
+#       },
+#       {
+#          "name":"Sergey Korsakov",
+#          "craft":"ISS"
+#       },
+#       {
+#          "name":"Kjell Lindgren",
+#          "craft":"ISS"
+#       },
+#       {
+#          "name":"Bob Hines",
+#          "craft":"ISS"
+#       },
+#       {
+#          "name":"Samantha Cristoforetti",
+#          "craft":"ISS"
+#       },
+#       {
+#          "name":"Jessica Watkins",
+#          "craft":"ISS"
+#       }
+#    ]
+# }
--- 12_weatherAPI.py
+++ 12_weatherAPI.py
@@ -3,8 +3,8 @@ import requests
 api_key = "7939dab06210394b8dfd6c750c368799"
 lat = str(28.538336)
 lon = str(-81.379234)
-city_name = 'orlando'
-api_url = "https://api.openweathermap.org/data/2.5/weather?q="+city_name+"&appid="+api_key
+city_name = 'villa urquiza'
+api_url = "https://api.openweathermap.org/data/2.5/weather?q="+city_name+"&appid="+api_key+"&units=metric" 
 
 #OLD API URL: "https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid="+api_key
 
@@ -12,4 +12,13 @@ request = requests.get(api_url)
 
 json = request.json()
 
-print(json)
+description = json.get("weather")[0].get("description")
+temp = str(json.get("main").get("temp"))
+tempMAX = str(json.get("main").get("temp_max"))
+tempMIN = str(json.get("main").get("temp_min"))
+
+print("Today's forecast is " + description)
+print("TEMP for today is " + temp)
+print("the MAX temp is " +tempMAX)
+print("the MIN temp is " +tempMIN)
+
