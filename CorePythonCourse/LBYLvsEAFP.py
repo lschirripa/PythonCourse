cd7a7ddf3e3f5d3e51df5bb4ce83b81bee9ad045
lschirripa <lucho.schirripa@gmail.com>
2022-05-09 18:17:08 -0300

    exceptions philosophy

---
+++ CorePythonCourse/LBYLvsEAFP.py
@@ -0,0 +1,21 @@
+# LOOK BEFORE YOU LEAP
+
+import os 
+
+p = '../python4begginers'
+
+if os.path.exists(p):     #what if the file exist but contains garbage? or is it only a dir and not a file? also could exist race condition here
+    process_file(p)
+else:
+    print(f'no such file as {p}')
+
+
+# EASIER TO ASK FORGIVENESS THAN PERMISSION
+
+p = '../python4begginers'
+
+try:
+    process_file(p)
+except OSError as e:
+    print(f'error: {e}')
+
