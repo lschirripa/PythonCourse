30fa29d8fbd0e37b28cb3c1b914554c508651561
lschirripa <lucho.schirripa@gmail.com>
2022-05-12 11:19:47 -0300

    update new files

---
+++ CorePythonCourse/16_generatorExpressions.py
@@ -0,0 +1,3 @@
+million_squares = (x*x for x in range(1,1000001))
+
+print(list(million_squares)[-10:])
---
+++ CorePythonCourse/17_any&all.py
@@ -0,0 +1,9 @@
+any&all
+
+any([False,True,False])
+
+all([True,False,False])
+
+#any(is_prime(x) for x in range(1300,1400))
+
+print(all(name == name.title() for name in ['London', 'Paris', 'Tokyo']))
