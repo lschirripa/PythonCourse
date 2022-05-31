387f0a30d7f02d6bae552848b81b2a3e17a36b6d
lschirripa <lucho.schirripa@gmail.com>
2022-05-30 23:10:12 -0300

    feat: add some functions extracted from the api

--- exploringApi.py
+++ exploringApi.py
@@ -3,14 +3,20 @@ import credentials
 
 client = Client(credentials.SID,credentials.token)
 
+#PRINT ALL THE MSGS
+for msg in client.messages.list():
+    print(msg.body)
+
+#DELETING ALL THE MSGS
 for msg in client.messages.list():
     print(f"Deleting {msg.body}")
     msg.delete()
 
-# msg = client.messages.create(
-#     from_= credentials.hidden_from,
-#     to = credentials.hidden_to,
-#     body= 'Hi there, sent from python!'
-# )
+#CREATING MSGS
+msg = client.messages.create(
+    from_= credentials.hidden_from,
+    to = credentials.hidden_to,
+    body= 'Hi there, sent from python!'
+)
 
 #print(f"Created a new message: {msg.sid}")
